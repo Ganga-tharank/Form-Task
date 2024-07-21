09484b3769e6df7d822804f6daba0d44ad1fb790
Ganga-tharank <154905189+Ganga-tharank@users.noreply.github.com>
2024-07-21 18:44:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,76 @@
-function appendToTable() {
-    var table = document.getElementById("datatable");
-    var newRow = table.insertRow(table.rows.length);
+function submitForm() {
+    let firstNameInput = document.getElementById("first-name");
+    let lastNameInput = document.getElementById("last-name");
+    let addressInput = document.getElementById("address");
+    let pincodeInput = document.getElementById("pincode");
+    let genderInputs = document.getElementsByName("gender");
+    let foodChoiceInput = document.getElementById("food-choice");
+    let stateInput = document.getElementById("state");
+    let countryInput = document.getElementById("country");
 
-    var firstName = document.getElementById("firstname").value;
-    var lastName = document.getElementById("lastname").value;
-    var address = document.getElementById("address").value;
-    var pincode = document.getElementById("pincode").value;
-    var gender = document.querySelector('input[name="gender"]:checked').value;
-    var foodChoices = document.querySelectorAll('input[name="food"]:checked');
-    var foodValues = Array.from(foodChoices).map(choice => choice.value);
-    var state = document.getElementById("state").value;
-    var country = document.getElementById("country").value;
-    
+    // Retrieve values from the input fields
+    let firstName = firstNameInput.value;
+    let lastName = lastNameInput.value;
+    let address = addressInput.value;
+    let pincode = pincodeInput.value;
+    let gender = getSelectedRadioValue(genderInputs);
+    let foodChoice = foodChoiceInput.value;
+    let state = stateInput.value;
+    let country = countryInput.value;
 
-    var cell1 = newRow.insertCell(0);
-    var cell2 = newRow.insertCell(1);
-    var cell3 = newRow.insertCell(2);
-    var cell4 = newRow.insertCell(3);
-    var cell5 = newRow.insertCell(4);
-    var cell6 = newRow.insertCell(5);
-    var cell7 = newRow.insertCell(6);
-    var cell8 = newRow.insertCell(7);
-    var cell9 = newRow.insertCell(8);
+    // Validate if first name and last name are provided
+    if (firstName && lastName) {
+        // Create a new row in the table
+        let table = document.getElementById("dataTable");
+        let newRow = table.insertRow(table.rows.length);
 
+        // Insert cells with data
+        let cell1 = newRow.insertCell(0);
+        let cell2 = newRow.insertCell(1);
+        let cell3 = newRow.insertCell(2);
+        let cell4 = newRow.insertCell(3);
+        let cell5 = newRow.insertCell(4);
+        let cell6 = newRow.insertCell(5);
+        let cell7 = newRow.insertCell(6);
+        let cell8 = newRow.insertCell(7);
 
-    cell1.innerHTML = firstName;
-    cell2.innerHTML = lastName;
-    cell3.innerHTML = address;
-    cell4.innerHTML = pincode;
-    cell5.innerHTML = gender;
-    cell6.innerHTML = foodValue.join(', ');
-    cell7.innerHTML = state;
-    cell8.innerHTML = country;
-    cell9.innerHTML = '<button onclick="deleteRow(this)">Delete</button>';
+        // Populate cells with input values
+        cell1.innerHTML = firstName;
+        cell2.innerHTML = lastName;
+        cell3.innerHTML = address;
+        cell4.innerHTML = pincode;
+        cell5.innerHTML = gender;
+        cell6.innerHTML = foodChoice;
+        cell7.innerHTML = state;
+        cell8.innerHTML = country;
 
-    clearForm();
+        // Clear form inputs
+        firstNameInput.value = "";
+        lastNameInput.value = "";
+        addressInput.value = "";
+        pincodeInput.value = "";
+        resetRadioButtons(genderInputs);
+        foodChoiceInput.value = "";
+        stateInput.value = "";
+        countryInput.value = "";
+    } else {
+        alert("Please fill in all required fields...");
+    }
 }
 
-
-function clearForm(){
-    document.getElementById("myForm").reset();
+// Helper function to get the value of the selected radio button
+function getSelectedRadioValue(radioInputs) {
+    for (let i = 0; i < radioInputs.length; i++) {
+        if (radioInputs[i].checked) {
+            return radioInputs[i].value;
+        }
+    }
+    return null; // Return null if no radio button is selected
 }
 
-
-function deleteRow(button) {
-    var row = button.parentNode.parentNode;
-    row.parentNode.removeChild(row);
+// Helper function to reset radio buttons
+function resetRadioButtons(radioInputs) {
+    for (let i = 0; i < radioInputs.length; i++) {
+        radioInputs[i].checked = false;
+    }
 }
